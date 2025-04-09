a3cc58f6ff2db076018f970c365c1ec7d468e4a2
ahm4dd <ahmed.emad.edu@gmail.com>
2025-04-09 17:50:00 +0300

    Added docstrings and tests.py to tests Maze for now

---
+++ src/tests.py
@@ -0,0 +1,67 @@
+import unittest 
+from core.maze import Maze
+from core.gui import Window
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    def test_maze_create_cells_large(self):
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+    def test_maze_draw_cell(self):
+        num_cols = 15
+        num_rows = 13
+        m1 = Maze(Window(800, 600),0, 0, num_rows, num_cols, 20, 30)
+        """
+        The logic is :
+        x1 = self._x1 + i * self._cell_size_x # Calculate the x-coordinate of the top left corner of the cell
+        y1 = self._y1 + j * self._cell_size_y # Calculate the y-coordinate of the top left corner of the cell
+        x2 = x1 + self._cell_size_x # Calculate the x-coordinate of the bottom right corner of the cell
+        y2 = y1 + self._cell_size_y # Calculate the y-coordinate of the bottom right corner of the cell
+        self._cells[i][j].draw(x1, x2, y1, y2)
+        self._animate()
+        """
+        for i in range(m1._num_cols):
+            for j in range(m1._num_rows):
+                x1 = 0 + i * 20 # Calculate the x-coordinate of the top left corner of the cell
+                y1 = 0 + j * 30 # Calculate the y-coordinate of the top left corner of the cell
+                x2 = x1 + 20 # Calculate the x-coordinate of the bottom right corner of the cell
+                y2 = y1 + 30 # Calculate the y-coordinate of the bottom right corner of the cell
+                self.assertEqual(
+                    m1._cells[i][j]._x1,
+                    x1,
+                )
+                self.assertEqual(
+                    m1._cells[i][j]._y1,
+                    y1,
+                )
+                self.assertEqual(
+                    m1._cells[i][j]._x2,
+                    x2,
+                )
+                self.assertEqual(
+                    m1._cells[i][j]._y2,
+                    y2,
+                )
+
+if __name__ == '__main__':
+    unittest.main()
